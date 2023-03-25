b4f30edcb252c278b8fe63aa299774a473c59595
@OsvaldoMoraes61 <110261126+OsvaldoMoraes61@users.noreply.github.com>
2023-03-25 18:00:54 -0300

    Aula 11

---
+++ Aula11/ex08.js
@@ -0,0 +1,12 @@
+var vel = 80
+console.log(`A velocidade de seu carro é de ${vel} Km/h`);
+console.log('Use cinto de segurança quando dirigir');
+console.log('Se dirigir não beba')
+//console.log('Você bebeu e esta dirigindo. Teje PRESO!')
+
+if (vel > 80 ){
+    console.log('Voce ultrapassou a velocidade maxima: MULTADO!')
+} else {
+    console.log('Você está dirigindo muito bem dentro da velocidade sinalizada');
+
+}
