e620ff68152f3b899ff2bb3ad4b67d645dba8769
@OsvaldoMoraes61 <110261126+OsvaldoMoraes61@users.noreply.github.com>
2023-04-07 19:25:49 -0300

    Atu Ex 16 e 17

--- Aula14ex/ex16/script.js
+++ Aula14ex/ex16/script.js
@@ -1,1 +1,30 @@
-alert('Olá, mundo!')
+function contar() {
+  let n1 = document.getElementById("ini")
+  let n2 = document.getElementById("fim")
+  let n3 = document.getElementById("pas")
+  let res = document.getElementById("res")
+ 
+  if (n1.value.length == 0 || n2.value.length == 0 || n3.value.length == 0) {
+    alert("Digite um nr., faltam dados ")
+  } else {
+    let ni = Number(n1.value)
+    let nf = Number(n2.value)
+    let np = Number(n3.value)
+    res.innerHTML = ''
+    if (np == 0) {
+      alert("O Passo não pode ser ZERO, então Passo=1")
+      np = 1
+    }
+
+    if (ni < nf) {
+      for (var nr = ni; nr <= nf; nr += np) {
+        res.innerHTML += ` ${nr} \u{1F449}`
+      }
+    } else {
+          for (let nr = ni; nr >= nf; nr -= np) {
+        res.innerHTML += ` ${nr} \u{1F449}`
+      }
+    }
+    res.innerHTML += `\u{1F3C1}`
+  }
+}
--- Aula14ex/ex17/script.js
+++ Aula14ex/ex17/script.js
@@ -1,1 +1,22 @@
-alert('Olá, mundo!')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    let mens = 'digite um valor para Número' 
+
+    if (num.value.length == 0 ) {
+        alert(mens )
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = mens
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+
+        }
+    }
+
+}
