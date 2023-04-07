a213d23f41de4bff4bf0a04ae3b59edf0a828a06
@OsvaldoMoraes61 <110261126+OsvaldoMoraes61@users.noreply.github.com>
2023-04-07 10:00:49 -0300

    Aula13 e Aula14

---
+++ Aula13/ambiente.js
@@ -0,0 +1,13 @@
+var contador = 1
+
+while (contador <= 6) {
+    console.log(`Bom dia nº ${contador}`)
+    contador++
+}
+
+/*
+console.log('Bom dia')
+console.log('Bom dia')
+console.log('Bom dia')
+console.log('Bom dia')
+*/
---
+++ Aula14/ambiente.js
@@ -0,0 +1,5 @@
+console.log('inicío')
+for (var c = 1; c <=4; c++){
+    console.log(c)
+}
+console.log('Término')
